111c335ff865ac3c88608c1d8d1b45d93471b64b
Siddarth Banerjee <siddarth2801@gmail.com>
2025-03-30 03:30:18 +0530

    added authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,18 +1,13 @@
 import { Router } from 'express';
 
+import { getUsers, getUser } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
+
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-  res.send({
-    title: 'Get all user',
-  });
-});
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) => {
-  res.send({
-    title: 'Get use details',
-  });
-});
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) => {
   res.send({
